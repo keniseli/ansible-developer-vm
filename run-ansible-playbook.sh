e85dc5ab76fcfb8a84335d417236d8cf9b5d1260
p0wnbauer <martin.ponbauer@hotmail.com>
2017-11-02 18:33:54 +0100

    Delete IDE related files from branch, Add comments run-ansible-playbook.sh

--- run-ansible-playbook.sh
+++ run-ansible-playbook.sh
@@ -47,7 +47,9 @@ check_and_install_sshpass() {
     fi
 }
 
+# This is installing all specified roles from 'requirements.yml' within the root directory
 install_roles_from_ansible_galaxy(){
+    step "installing roles from ansible-galaxy ..."
    ansible-galaxy install --force -r /vagrant/requirements.yml
 }
 
