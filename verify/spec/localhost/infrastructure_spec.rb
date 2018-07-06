d76131217efcb3651812dfea0686134715cc7e3c
keniseli <ken.iseli@gmail.com>
2018-07-06 17:36:22 +0200

    Adjusted docker version in test

--- verify/spec/localhost/infrastructure_spec.rb
+++ verify/spec/localhost/infrastructure_spec.rb
@@ -22,7 +22,7 @@ describe package('nodejs') do
   it { should be_installed }
 end
 
-describe service('docker') do
-  it { should be_enabled }
-  it { should be_running }
+describe command('docker --version') do
+  its(:stdout) { should contain('18.03.01') }
+  its(:stderr) { should match '' }
 end
